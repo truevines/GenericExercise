d8092fada70a09c4462bcf9faf4595151a9553c1
DuDuA <DuDuA>
2016-01-09 00:47:34 -0800

    adding isomorph string

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -6,3 +6,23 @@ def isomorph(s, t):
 	if (len(s)!=len(t)):
 		return False
 
+	dic={}
+
+	# s as the key; t as the value
+	i=0
+	while (i<len(s)):
+	# i-th character:
+		if (s[i] not in dic):
+			dic[s[i]]=t[i]
+
+		elif (dic[s[i]]!=t[i]):
+				return False
+
+		else: # go on
+			i+=1
+	return True
+
+
+
+
+
